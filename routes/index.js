9711375fb9ce7380b4a82168a2cbdd2dc5a6a951
kevin <huke@innobuddy.com>
2016-06-20 13:01:22 +0800

    测试纯json输出

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ const router = new Router();
 
 
 router.get('/', async (ctx, next) => {
-  ctx.state = {
-    title: 'koa2 title'
-  };
+  // ctx.state = {
+  //   title: 'koa2 title'
+  // };
 
+  let data = [{
+    name: "huke",
+    age: "25",
+    data: [{
+      nothing: "none"
+    }]
+  }]
   await ctx.render('index', {
+    data: JSON.stringify(data)
   });
 })
 module.exports = router;
