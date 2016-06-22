7f94f392f1e67d8ee29b013787b5c11dfdc06470
kevin <huke@innobuddy.com>
2016-06-22 09:23:32 +0800

    增加27017端口数据库

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const Koa = require('koa');
 const app = new Koa();
 const views = require('koa-views');
 const onerror = require('koa-onerror');
+const mongoose = require('mongoose');
 
 const router = require('./common/router');
+const dbConfig = require('./config/db');
+
+mongoose.connect('mongodb://' + dbConfig.host + ':' + dbConfig.port);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('DB connected on ' + 'mongodb://' + dbConfig.host + ':' + dbConfig.port);
+});
+
+// TODO: 需增加监视进程，当应用终止是关闭数据库
 
 // middlewares
 app.use(require('koa-static')(__dirname + '/public'));
